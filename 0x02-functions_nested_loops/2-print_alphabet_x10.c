374be94b6352616350897163553b0164287f3d9e
youssef3332-a11y <youssef17102003@gmail.com>
2023-09-19 17:47:13 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	i = (int)'a';
+	for (i; i <= (97 + 26); i++)
+	{
+		putchar('i');
+	}
+	putchar('\n');
+}
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
