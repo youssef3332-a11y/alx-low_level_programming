f731c7a527e50ff935905b77f6431bd1684f56d7
youssef3332-a11y <youssef17102003@gmail.com>
2023-09-19 20:19:51 -0700

    task 4 and 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - ps
+ * @n: number
+ * Return: 1 or 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar('0');
+		return (0);
+	}
+	else
+	{
+		putchar('-');
+		return (-1);
+	}
+}
