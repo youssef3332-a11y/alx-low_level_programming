8f9ac36647ad0ce102a9663baf254d9c67aac6aa
youssef3332-a11y <youssef17102003@gmail.com>
2023-09-20 18:43:50 -0700

    task 101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - .
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int s, i;
+
+	s = 0;
+	i = 0;
+	while (i < 1024)
+	{
+		if (((i % 3) == 0) || ((i % 5) == 0))
+		{
+			s += i;
+		}
+	}
+	printf("%d\n", s);
+}
