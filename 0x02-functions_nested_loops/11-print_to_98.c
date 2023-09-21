e69934a1f918a67fabade11523fd36c0ce7fa906
youssef3332-a11y <youssef17102003@gmail.com>
2023-09-20 18:06:39 -0700

    go to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_to_98 - to 98
+ * @n: the start
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	int i, j;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else
+	{
+		for (j = n; j >= 98; j--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+}
