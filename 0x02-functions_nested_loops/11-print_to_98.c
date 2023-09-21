9da5d3889bcb7327b7119cdfc11f840069392e83
youssef3332-a11y <youssef17102003@gmail.com>
2023-09-20 18:22:45 -0700

    go to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,7 +15,7 @@ void print_to_98(int n)
 			{
 				printf(", ");
 			}
-			n++
+			n++;
 		}
 	}
 	else
@@ -27,7 +27,7 @@ void print_to_98(int n)
 			{
 				printf(", ");
 			}
-			n--
+			n--;
 		}
 	}
 }
