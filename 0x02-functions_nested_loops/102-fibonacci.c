b5aa77c58bca743fcbba8c923e55ec9e27425e6f
youssef3332-a11y <youssef17102003@gmail.com>
2023-09-22 19:42:20 -0700

    Fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main
+ *
+ *Return: 0
+ */
+void main(void)
+{
+	int i = 1, u2, u1 = 2, u = 1;
+
+	printf("%d, %d", u, u1);
+	while (i < 49)
+	{
+		u2 = u1 + u;
+		printf(", %d", u2);
+		u = u1;
+		u1 = u2;
+		i++;
+	}
+	printf("\n");
+}
