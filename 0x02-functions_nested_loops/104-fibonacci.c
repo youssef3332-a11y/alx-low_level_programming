bc04f5dcdd558c19c22e56c75b973732eb5e8dcf
youssef3332-a11y <youssef17102003@gmail.com>
2023-09-22 20:26:03 -0700

    task 104

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	long int i = 1, u2, u1 = 2, u = 1;
+
+	printf("%ld, %ld", u, u1);
+	while (i < 97)
+	{
+		u2 = u1 + u;
+		printf(", %ld", u2);
+		u = u1;
+		u1 = u2;
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
