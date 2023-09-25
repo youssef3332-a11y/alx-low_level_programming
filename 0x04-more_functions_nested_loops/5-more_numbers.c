ad99fa4c39ed2340467cb13d064701a95c6441cb
youssef3332-a11y <youssef17102003@gmail.com>
2023-09-25 12:27:24 -0700

    new files

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,9 +9,13 @@ void more_numbers(void);
 
 	for (j = 0; j < 10; j++)
 	{
-		for (i = '0'; i <= '14'; i++)
+		for (i = 0; i <= 14; i++)
 		{
-			_putchar(i);
+			if (i >= 10)
+			{
+				_putchar('1');
+			}
+			_putchar((i % 10) + '0');
 		}
 		_putchar('\n');
 	}
