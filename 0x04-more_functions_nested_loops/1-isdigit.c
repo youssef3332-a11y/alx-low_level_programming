4e12d23fab34a4227d3483ddbb0425190b9ca72e
youssef3332-a11y <youssef17102003@gmail.com>
2023-09-24 21:08:55 -0700

    new file

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isdigit - is digit
+ * @c: c
+ * Return: 1
+ * Return:0
+ */
+int _isdigit(int c)
+{
+	if ((c <= '9') && (c >= '0'))
+	{
+		return (1);
+	}
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,2 +1,3 @@
 int putchar(int character);
 int _isupper(int c);
+int _isdigit(int c);
