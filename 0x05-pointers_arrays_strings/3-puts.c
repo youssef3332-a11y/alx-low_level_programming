5e590ce0e2cec1d6fe769440255c5d160b40bc63
youssef3332-a11y <youssef17102003@gmail.com>
2023-09-28 07:23:07 -0700

    new file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - p
+ * @str: s
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev -r
+ * @s : c
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int *i;
+
+	i = (s + _strlen(s) - 2);
+	while (i != s)
+	{
+		_putchar(*i--);
+	}
+}
