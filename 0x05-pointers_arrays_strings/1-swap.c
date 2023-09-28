00b23d0ebbce5a0801ccdf24a7ee597b978d351a
youssef3332-a11y <youssef17102003@gmail.com>
2023-09-28 06:34:50 -0700

    new file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+nclude "main.h"
+/*
+ * swap_int - p
+ * @a: a
+ * @b: b
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*
+ * _strlen - s
+ * @s: s
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
