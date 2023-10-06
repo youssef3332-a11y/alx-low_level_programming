5e11cca9eb8ee1ad6af3e848ef0fdea03b3791ca
youssef3332-a11y <youssef17102003@gmail.com>
2023-10-06 10:14:37 -0700

    new file

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,23 +7,11 @@
  */
 void puts2(char *str)
 {
-	int longi = 0;
-	int t = 0;
-	char *y = str;
-	int o;
+	int i = 0;
 
-	while (*y != '\0')
+	while (s[i] != '\0')
 	{
-		y++;
-		longi++;
-	}
-	t = longi - 1;
-	for (o = 0 ; o <= t ; o++)
-	{
-		if (o % 2 == 0)
-	{
-		_putchar(str[o]);
-	}
+		_putchar(s[i++]);
 	}
 	_putchar('\n');
 }
