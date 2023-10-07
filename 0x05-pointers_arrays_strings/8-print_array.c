8a2e88b95107cd683ae6c203a1c3b9c185931972
youssef3332-a11y <youssef17102003@gmail.com>
2023-10-07 06:40:39 -0700

    new

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,9 +14,10 @@ void print_array(int *a, int n)
 	{
 		if (i == n - 1)
 		{
-			printf("%d\n", a[n - 1]);
+			printf("%d", a[n - 1]);
 			break;
 		}
 		printf("%d, ", a[i++]);
 	}
+	printf("\n");
 }
