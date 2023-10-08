ecc360914cde7ddeb3c04b8679171b27228c4358
youssef3332-a11y <youssef17102003@gmail.com>
2023-10-08 10:18:04 -0700

    new file

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _strncat - p
+ * @dest: n
+ * @src: 2
+ * @n: b
+ * Return: char
+ */
+*_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0, f = 0;
+	int len1 = 0, len2 = 0;
+
+	while (dest[i] != '\0')
+	{
+		len1++;
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		len2++;
+		j++;
+	}
+	if (len2 > n)
+	{
+		len2 = n;
+	}
+	while (f < len2)
+	{
+		*(dest + len1 + f) = src[f];
+		f++;
+	}
+	return (dest);
+}
