978e2124b9a21ad53fd8dcb1dcb8f07f9fdfe1b9
youssef3332-a11y <youssef17102003@gmail.com>
2023-10-10 05:40:20 -0700

    new file

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen_recursion - p
+ * @s: s
+ * Return: int
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
