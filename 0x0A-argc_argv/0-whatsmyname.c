7eeba646417ee939cc3cc916d4ff370b6690f404
youssef3332-a11y <youssef17102003@gmail.com>
2023-10-14 09:04:32 -0700

    new file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,8 @@
  * @argv: arg vector
  * Return: void
  */
-void main(--attribute--((unused)) int argc, char *argv[])
+int main(--attribute--((unused)) int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
+	return (0);
 }
