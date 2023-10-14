b0c451c3429e9b99f3336002cf07cefbb9cb14b9
youssef3332-a11y <youssef17102003@gmail.com>
2023-10-14 09:51:33 -0700

    new file

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - prints all arguments it receives
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", atoi(argv[1]) + atoi(argv[2]));
+		return (0);
+	}
+}
