a3fd3ef4cac693becf8af035f3bd8fd1149e5842
youssef3332-a11y <youssef17102003@gmail.com>
2023-10-14 09:32:19 -0700

    new file

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,11 +5,12 @@
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return:void
+ * Return: 0
  */
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	(void)argv;
 
 	printf("%d\n", argc - 1);
+	return (0);
 }
