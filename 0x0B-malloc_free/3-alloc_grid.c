2c5eaa01c48570365e493031c5c0655d19f1fbf8
youssef3332-a11y <youssef17102003@gmail.com>
2023-10-28 07:20:29 -0700

    new

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -5,27 +5,40 @@
  * @height: The height of the grid.
  * Return: A pointer to the 2D array.
  */
-int **alloc_grid(int width, int height) 
-{        
+int **alloc_grid(int width, int height)
+{
 	int **grid;
-	int i;
+	int i, j;
 
 	if (width <= 0 || height <= 0)
-	{        
+	{
 		return (NULL);
-	} 
+	}
 	grid = malloc(sizeof(int *) * height);
 	if (grid == NULL)
-	{        
+	{
 		return (NULL);
-        }        
+	}
 	for (i = 0; i < height; i++)
 	{
-	grid[i] = calloc(width, sizeof(int));
-	if (grid[i] == NULL)
-	{
-		return (NULL);
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			for (i-- ; i >= 0; i--)
+			{
+				free(grid[i]);
+			}
+			free(grid);
+			return (NULL);
+		}
 	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
 	}
 	return (grid);
+
 }
