8484c16d56134d74deb58655d6a3927d4b646a6d
youssef3332-a11y <youssef17102003@gmail.com>
2023-12-09 08:24:00 -0800

    new file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 /**
  * init_dog - dog
@@ -10,6 +11,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		d = malloc(sizeof(struct dog));
+	}
 	d->name = "Poppy";
 	d->age = 3.5;
 	d->owner = "Bob";
