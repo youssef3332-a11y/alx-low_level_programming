439dcc5d77a1f7d59461c9e13d29d9b6307cd614
youssef3332-a11y <youssef17102003@gmail.com>
2023-12-23 08:35:21 -0800

    new file

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include "variadic_functions.h"
 /**
  * sum_them_all - p
  * @n: n
@@ -9,21 +10,17 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	int s = 0;
+	unsigned int i;
+	va_list array;
 
 	if (n == 0)
-	{
 		return (0);
-	}
-	else
-	{
-		va_list array;
+	va_start(array, n);
 
-		va_start(array, n);
-		for (int i = 0; i < n; i++)
-		{
-			s += va_arg(array, int);
-		}
-		va_end(array);
+	for (i = 0; i < n; i++)
+	{
+		s += va_arg(array, int);
 	}
+	va_end(array);
 	return (s);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef VARIADIC
 #define VARIADIC
-
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+
 #endif
