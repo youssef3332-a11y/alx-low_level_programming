8c8b4fc79c8fbc195232507ccd06bc7ced3739d9
youssef3332-a11y <youssef17102003@gmail.com>
2023-12-23 09:21:36 -0800

    new file

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_strings - p
+ * @separator: s
+ * @n:n
+ * @...: .
+ * Return: viod
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list array;
+	char *str;
+
+	va_start(array, n);
+	for (i = 0; i < n; i++)
+	{
+		if (i != 0 && separator != NULL)
+			printf("%s", separator);
+		str = va_arg(array, char *);
+		if (str == NULL)
+			printf("%s", str);
+		printf("%s", str);
+	}
+	va_end(array);
+	printf("\n");
+}
