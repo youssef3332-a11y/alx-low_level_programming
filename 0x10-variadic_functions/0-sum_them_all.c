af9b88bde3d23b2a056c63bdc6dacf32ac635ca3
youssef3332-a11y <youssef17102003@gmail.com>
2023-12-23 08:18:01 -0800

    new file

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * sum_them_all - p
+ * @n: n
+ * @...: numbers
+ * Return: int
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int s = 0;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		va_list array;
+
+		va_start(array, n);
+		for (int i = 0; i < n; i++)
+		{
+			s += va_arg(array, int);
+		}
+	}
+	va_end(array);
+	return (s);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,5 @@
+#ifndef VARIADIC
+#define VARIADIC
+
+int sum_them_all(const unsigned int n, ...);
+#endif
